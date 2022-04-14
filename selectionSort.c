c5a02d37235fb97a4228fd00889a23cab7de30f6
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-04-15 08:34:55 +0900

    Create selectionSort.c

---
+++ selectionSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+	
+	int arr[10] = {3, 5, 0, 0, 1, 2, 7};
+	int n = sizeof(arr) / sizeof(int);
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = i+1; j < n; j++) {
+			if( arr[i] > arr[j]) {
+				int data = arr[i];
+				arr[i] = arr[j];
+				arr[j] = data;
+			}
+		}
+	}
+	
+	for(int i = 0; i < n; i++) {
+		printf("%d, ", arr[i]);
+	}
+	return 0;
+}
