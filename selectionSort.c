62abd7c2b09f3d099237d3384a4ad89548d2c6b9
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-08-23 12:20:54 +0900

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
- 
+
 int main() {
 	
-	int arr[10] = {3, 5, 0, 0, 1, 2, 7};
-	int n = sizeof(arr) / sizeof(int);
+	int arr[10] = {3, 5, 0, 0, 1, 2, 7}; // 정수형 arr 배열의 버퍼를 10만큼 할당후 초기화 
+	int n = sizeof(arr) / sizeof(int); // arr의 크기를 정수형 n변수에 할당
 	
-	for(int i = 0; i < n; i++) {
+	for(int i =0; i < n; i++) {
 		for(int j = i+1; j < n; j++) {
-			if( arr[i] > arr[j]) {
-				int data = arr[i];
-				arr[i] = arr[j]; // swap
-				arr[j] = data;
+			if( arr[i] > arr[j]) { // arr[i]의 인덱스이 더크면 Ture
+				arr[i] ^= arr[j] ^= arr[i] ^= arr[j]; // swap
 			}
 		}
 	}
 	
-	for(int i = 0; i < n; i++) {
+	for(int i =0; i < n; i++) {
 		printf("%d, ", arr[i]);
 	}
 	return 0;
